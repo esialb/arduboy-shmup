8334c872ffd1fc8c08551372ccb9d92d1383fbde
Robin Kirkman <robin.kirkman@gmail.com>
2016-12-06 01:28:02 -0500

    use faster sprite draw

--- src/Sprites.cpp
+++ src/Sprites.cpp
@@ -9,30 +9,30 @@
 #include <avr/pgmspace.h>
 
 const unsigned char Sprites::PLAYER[] PROGMEM = {
-		0b01100000,
-		0b01010000,
-		0b01011110,
+		0b00011000,
+		0b11100111,
 		0b10000001,
-		0b10000001,
-		0b01011110,
-		0b01010000,
-		0b01100000,
+		0b01000010,
+		0b00100100,
+		0b00100100,
+		0b00100100,
+		0b00011000,
 };
 
 const unsigned char Sprites::PLAYER_MASK[] PROGMEM = {
-		0b01100000,
-		0b01110000,
-		0b01111110,
+		0b00011000,
 		0b11111111,
 		0b11111111,
 		0b01111110,
-		0b01110000,
-		0b01100000,
+		0b00111100,
+		0b00111100,
+		0b00111100,
+		0b00011000,
 };
 
 void Sprites::draw(Arduboy &arduboy, const unsigned char *sprite, const unsigned char *mask, int x, int y) {
 	if(mask)
-		arduboy.drawSlowXYBitmap(x, y, mask, 8, 8, WHITE);
+		arduboy.drawBitmap(x, y, mask, 8, 8, WHITE);
 	if(sprite)
-		arduboy.drawSlowXYBitmap(x, y, sprite, 8, 8, BLACK);
+		arduboy.drawBitmap(x, y, sprite, 8, 8, BLACK);
 }
