2a7d23b5c2d89a30626d11bddeaeb09c22472688
Robin Kirkman <robin.kirkman@gmail.com>
2017-03-01 11:02:10 -0500

    fastert collision detection

--- src/Sprites.cpp
+++ src/Sprites.cpp
@@ -84,30 +84,22 @@ void Sprites::draw(Arduboy &arduboy, const uint8_t *sprite, const uint8_t *mask,
 }
 
 bool Sprites::collides(int x1, int y1, const uint8_t *m1, int x2, int y2, const uint8_t *m2) {
-	uint8_t buf1[8], buf2[8];
 	if(x1 - x2 >= 8 || x2 - x1 >= 8 || y1 - y2 >= 8 || y2 - y1 >= 8)
 		return false;
-	memcpy_P(buf1, m1, 8);
 	int xoff = x2 - x1;
 	int yoff = y2 - y1;
-	memset(buf2, 0, 8);
-	if(xoff == 0)
-		memcpy_P(buf2, m2, 8);
-	else if(xoff > 0) {
-		memcpy_P(buf2 + xoff, m2, 8 - xoff);
-	} else {
-		memcpy_P(buf2, m2 + -xoff, 8 - -xoff);
-	}
-	if(yoff > 0) {
-		for(int i = 0; i < 8; i++)
-			buf2[i] = buf2[i] << yoff;
-	} else if(yoff < 0) {
-		for(int i = 0; i < 8; i++)
-			buf1[i] = buf1[i] << -yoff;
-	}
-	for(int i = 0; i < 8; i++) {
-		if(buf1[i] & buf2[i])
+
+	for(int i = 0; i < 8 - abs(xoff); i++) {
+		uint8_t b1, b2;
+		b1 = pgm_read_byte(m1 + i + (xoff > 0 ? xoff : 0));
+		b2 = pgm_read_byte(m2 + i + (xoff < 0 ? -xoff : 0));
+		if(yoff > 0)
+			b2 = b2 << yoff;
+		if(yoff < 0)
+			b1 = b1 << -yoff;
+		if(b1 & b2)
 			return true;
 	}
+
 	return false;
 }
